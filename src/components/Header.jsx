76870666768d2be794a35a9cd082afa5893ec163
hendry <hendrry16@gmail.com>
2025-08-08 17:38:54 +0700

    make test passed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 import Navigation from './Navigation';
 import '../styles/Header.css';
 
-function Header({ }) {
+function Header({ logout }) {
   return (
     <header className="flex items-center justify-between px-4 py-2 bg-white/30 text-black h-16 border">
       <Link to="/">
         <h1 className="text-2xl text-gray-800 font-semibold">Threads</h1>
       </Link>
-      {logout && <Navigation logout={} />}
+      {logout && <Navigation logout={logout} />}
     </header>
   );
 }
